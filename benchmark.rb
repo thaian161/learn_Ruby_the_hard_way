6d94e0275bbd41455aac9ab36f6241ac5ce7e6ed
thaian161 <thaian161@yahoo.com>
2022-07-25 21:23:04 -0500

    complete ex29 and benchmark

---
+++ benchmark.rb
@@ -0,0 +1,16 @@
+def benchmark
+  start_time = Time.now
+  yield
+  end_time = Time.now
+  running_time = end_time - start_time
+end
+ 
+# Be careful, pasting this into IRB will take a long time to print.
+# It's a loooong string. :)
+long_string = "apple" * 100
+ 
+running_time = benchmark { long_string.reverse }
+puts "string.reverse took #{running_time} seconds to run"
+
+running_time = benchmark { long_string.downcase }
+puts "string.downcase took #{running_time} seconds to run"
